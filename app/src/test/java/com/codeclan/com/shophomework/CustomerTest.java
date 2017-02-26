947725449ac3ad5efff793b34831d32abd489684
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-26 18:37:20 +0000

    Customer test name

--- app/src/test/java/com/codeclan/com/shophomework/CustomerTest.java
+++ app/src/test/java/com/codeclan/com/shophomework/CustomerTest.java
@@ -1,8 +1,27 @@
 package com.codeclan.com.shophomework;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 /**
  * Created by user on 26/02/2017.
  */
 
 public class CustomerTest {
+
+    Customer customer1;
+    Customer customer2;
+
+    @Before
+    public void before() {
+        customer1 = new Customer("Pierre", 300);
+        customer2 = new Customer("RayJay", 400);
+
+    }
+
+    @Test
+    public void customerHasName() {
+        assertEquals("Pierre", customer1.getName());
+    }
 }
