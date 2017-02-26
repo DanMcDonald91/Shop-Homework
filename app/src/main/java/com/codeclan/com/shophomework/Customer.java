d0f6043ff6f450c7df8a6f435aeb2e610f5c8e95
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-26 17:53:49 +0000

    Added getters and setters for Customer class

--- app/src/main/java/com/codeclan/com/shophomework/Customer.java
+++ app/src/main/java/com/codeclan/com/shophomework/Customer.java
@@ -5,4 +5,30 @@ package com.codeclan.com.shophomework;
  */
 
 public class Customer {
+
+    private String name;
+    private int cash;
+
+    public Customer(String name, int cash) {
+
+        this.setName(name);
+        this.setCash(cash);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCash() {
+        return cash;
+    }
+
+    public void setCash(int cash) {
+        this.cash = cash;
+    }
 }
