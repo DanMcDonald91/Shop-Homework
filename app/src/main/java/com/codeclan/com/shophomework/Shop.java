b5f3a88af7b108cf90606d18fc5557a25b9e76a0
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-02-26 22:04:20 +0000

    Set up shop class

--- app/src/main/java/com/codeclan/com/shophomework/Shop.java
+++ app/src/main/java/com/codeclan/com/shophomework/Shop.java
@@ -3,9 +3,14 @@ package com.codeclan.com.shophomework;
 /**
  * Created by user on 26/02/2017.
  */
-//
-//public class Shop {
-//
-//    private String name;
-//    private
-//}
+
+public class Shop {
+
+    private String name;
+    private int shopBalance;
+    private int sales;
+    private int refunds;
+
+    public Shop(String name, int shopBalance, int sales, int refunds)
+
+}
